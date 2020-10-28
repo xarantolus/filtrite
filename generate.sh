f670df5b920de348001b27832ab6d36c144e6950
xarantolus <xarantolus@protonmail.com>
2020-10-28 19:32:05 +0100

    Update description and also add default list

--- generate.sh
+++ generate.sh
@@ -19,9 +19,10 @@ chmod +x filtrite
 chmod +x deps/ruleset_converter
 mkdir -p dist
 
+log "Start generating bromite-default"
+./filtrite lists/bromite-default.txt dist/bromite-default.dat
+
 log "Start generating bromite-extended"
-
-
 ./filtrite lists/bromite-extended.txt dist/bromite-extended.dat
 
 cleanup
