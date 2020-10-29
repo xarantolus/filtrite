cc2ed3b7da7a28bc1993dd8ac9c12ebb57e96565
xarantolus <xarantolus@protonmail.com>
2020-10-29 18:55:03 +0100

    Better group names?

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-echo "::group::{Init}"
+echo "::group::Init"
 
 set -e
 
@@ -12,21 +12,22 @@ cleanup() {
 cleanup
 
 echo "::endgroup::"
-echo "::group::{Build executable}"
+echo "::group::Build executable"
 
 log "Building"
 
 go build -v -o filtrite
 
 echo "::endgroup::"
-echo "::group::{Other setup steps}"
+echo "::group::Other setup steps"
 # Last setup steps
 chmod +x filtrite
 chmod +x deps/ruleset_converter
 mkdir -p dist
 
 echo "::endgroup::"
-echo "::group::{List: bromite-default}"
+
+echo "::group::List: bromite-default"
 
 # Download default bromite filter list
 wget -O lists/bromite-default.txt https://raw.githubusercontent.com/bromite/filters/master/lists.txt
@@ -35,12 +36,13 @@ log "Start generating bromite-default"
 ./filtrite lists/bromite-default.txt dist/bromite-default.dat
 
 echo "::endgroup::"
-echo "::group::{List: bromite-extended}"
+
+echo "::group::List: bromite-extended"
 log "Start generating bromite-extended"
 ./filtrite lists/bromite-extended.txt dist/bromite-extended.dat
 
 echo "::endgroup::"
-echo "::group::{Cleanup}"
+echo "::group::Cleanup"
 
 cleanup
 echo "::endgroup::"
